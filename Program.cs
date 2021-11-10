d0f093c321e225e1ce9e59ecedbb26edc96f8b00
Lakeitra Coleman <colemantakiya@gmail.com>
2021-11-10 10:44:49 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PlayingWithVariablesConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declared and initialized a variable
+            //camelCase for the variable name
+            //explicit typing
+            string babyName = "Kyson";
+            int babyAge = 1;
+            char nickName = 'K';
+            bool lovesSleeping = true;
+            double nightLights = 3;
+            decimal weight = 19.2m;
+
+            Console.WriteLine($"My baby's name is {babyName}. He is a {babyAge} year old and he has {nightLights} night lights. It is {lovesSleeping} that he loves to sleep, which is why he is {weight} pounds now.");
+
+
+
+
+        }
+    }
+}
